689f8aa20e5230c89e5c4061b2c19efc00dd5e56
vishal vana <vishalvana592003@gmail.com>
2026-04-30 21:41:18 +0530

    Initial commit - BaaS Spring Boot setup

---
+++ src/main/java/com/vishal/baas_platform/BaasPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.vishal.baas_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaasPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaasPlatformApplication.class, args);
+	}
+
+}
